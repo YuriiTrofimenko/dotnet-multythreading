d29e327f92f37ea2eebc82dc9e8430c82b1ee0f0
student <student@PC_105_1->
2018-12-08 18:19:42 +0200

    init

---
+++ Threads/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1 */
+
+            /*new Thread(() => {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Hello ");
+                }
+            }).Start();
+
+            new Thread(() => {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("World");
+                }
+            }).Start();
+
+            Console.WriteLine("Start");*/
+
+            /* 2 */
+
+            /*Thread t1 = new Thread(() => {
+                for (int i = 0; i > -1; i++)
+                {
+                    Console.WriteLine("Hello ");
+                }
+            });
+
+            Thread t2 = new Thread(() => {
+                for (int i = 0; i > -1; i++)
+                {
+                    Console.WriteLine("World");
+                }
+            });
+
+            t1.IsBackground = true;
+            t2.IsBackground = true;
+
+            t1.Start();
+            t2.Start();
+
+            Console.WriteLine("Start--------------------------------");
+
+            t1.Join(50);
+            //t2.Join(500);
+            
+            //Thread.Sleep(1000);*/
+
+            /* 3 */
+            /*Thread t1 = new Thread(new Worker().doWork);
+            t1.Start();
+            Thread.Sleep(10);
+            t1.Interrupt();
+            Console.WriteLine("The End");*/
+
+            /* 4 */
+            /*Worker w1 = new Worker(true);
+            Thread t1 = new Thread(w1.doWork);
+            t1.Start();
+            Thread.Sleep(10);
+            w1.isActive = false;
+            Console.WriteLine("The End");*/
+
+            /* 5 */
+            Worker w1 = new Worker(true);
+            Thread t1 = new Thread(w1.doParamWork);
+            int number = 3;
+            string word1 = "Привет, ";
+            string word2 = "Мир";
+            string word3 = "!";
+            ArrayList list =
+                new ArrayList() { number, word1, word2, word3};
+            t1.Start(list);
+            //Thread.Sleep(10);
+            //w1.isActive = false;
+            Console.WriteLine("The End");
+        }
+    }
+}
---
+++ Threads/Worker.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    class Worker
+    {
+        public bool isActive = false;
+        public Worker(bool _isActive) {
+            isActive = _isActive;
+        }
+        public void doWork() {
+            for (int i = 0; i < 10; i++)
+            {
+                if (isActive)
+                {
+                    Thread.Sleep(1);
+                    Console.WriteLine("Hello ");
+                    Thread.Sleep(1);
+                    Console.WriteLine("World");
+                    Thread.Sleep(1);
+                    Console.WriteLine("!");
+                }
+                else {
+                    Thread.CurrentThread.Interrupt();
+                }
+            }
+        }
+
+        public void doParamWork(Object args)
+        {
+            //int number = (int)args;
+            ArrayList list = (ArrayList)args;
+            for (int i = 0; i < (int)list[0]; i++)
+            {
+                if (isActive)
+                {
+                    Thread.Sleep(1);
+                    Console.WriteLine(list[1]);
+                    Thread.Sleep(1);
+                    Console.WriteLine(list[2]);
+                    Thread.Sleep(1);
+                    Console.WriteLine(list[3]);
+                }
+                else
+                {
+                    Thread.CurrentThread.Interrupt();
+                }
+            }
+        }
+    }
+}
